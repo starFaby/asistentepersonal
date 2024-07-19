6e8c3e756623f1bd1fc3c49c3c32568354c317f5
starfaby <star._faby@hotmail.com>
2024-07-19 14:01:18 -0500

    agregando imagen en el html

--- src/client/controller/clientControllerModalProcesoVoz.py
+++ src/client/controller/clientControllerModalProcesoVoz.py
@@ -2,19 +2,32 @@ from src.heart.heartSistem import *
 from src.heart.heartServices import *
 from src.util.utilVoice import *
 class ClientControllerModalProcesoVoz:
+    def onGetClientControllerModalProceso(id):
+        try:
+            modProcVoz = ClientServiceModalProcesoVoz.ongetClientServiceModalProcesoVoz(id)
+            for item in modProcVoz:
+                print(item.pfsaprcsid)
+            procesoList = []
+            context = {
+                    'verModal' : True,
+                    'procesoList' : procesoList,
+                    'modProcVoz' : modProcVoz
+            }
+            return render("client/clientProceso.html", **context)
+        except SQLAlchemyError as e:
+            return render('errors/error500.html', e)
+    
     def onGetClientControllerModalProcesoVoz(id):
         try:
                 modProcVoz = ClientServiceModalProcesoVoz.ongetClientServiceModalProcesoVoz(id)
                 assisVoz = ''
+                idAux = 0
                 for item in modProcVoz:
                     assisVoz = item.pfsaprcsdetalle
+                    idAux = item.pfsapcasoid
                 print(type(assisVoz))
                 onGetMultiProccessingVoz(assisVoz)
-                procesoList = []
-                context = {
-                    'verModal' : True,
-                    'procesoList': procesoList
-                }
-                return render("client/clientProceso.html", **context)
+                return redirect(url_for('crp.onGetClientControllerProcesoListView', id=idAux))
+        #return redirect(url_for(crp.onGetClientControllerProcesoListView', id=item.pfsapcasoid))) 
         except SQLAlchemyError as e:
                 return render('errors/error500.html', e)
